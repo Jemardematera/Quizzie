c2b0afd6aadc361d29b9ea0c8a9bcadf054c3896
Jemardematera <81813634+Jemardematera@users.noreply.github.com>
2021-05-03 16:02:05 +0800

    Initial commit

---
+++ resources/views/Dashboardtc.blade.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quizzie</title>
+
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-navbar navbar-red bg- red" style="background-color: white;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Quizzie</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Your Library
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Study</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Create
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Study</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+
+    <div class="col-12 mt-5">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="panel-heading text-center color:red gray"><b> Total Of Student</b>
+
+                            </div>
+
+                        </div>
+                        <div class="card-body ">
+
+                            <blockquote class="blockquote mb-0">
+                                <div class="panel-heading text-center"><b> 01</b>
+
+                                </div>
+
+                            </blockquote>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="panel-heading text-center"><b> Total Of Subject</b>
+
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <div class="panel-heading text-center"><b> 01</b>
+
+                                </div>
+                            </blockquote>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-4 ">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="panel-heading text-center"><b>Pending Student</b>
+
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <div class="panel-heading text-center"><b> 01</b>
+
+                                </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+
+
+                <div class="col-6 - mt-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="panel-heading text-right"><b>Recently</b>
+
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+
+                            </blockquote>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+                <div class="col-6 mt-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="panel-heading text-right"><b>List of Student</b>
+
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+
+                            </blockquote>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/Home.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Quizzie</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-image: url("../images/bgkk.gif");
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: 100% 100%;
+                color: darkgray;
+                font-family: 'Century', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: bottom;
+            }
+
+            .title {
+                font-size: 50px;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+           
+            <div class="content">
+                <div class="title m-b-md">
+                    Quizzie
+                </div>
+
+                
+            </div>
+        </div>
+    </body>
+</html>
